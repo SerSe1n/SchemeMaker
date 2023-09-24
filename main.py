75e6327f47b9470d72f2dca80b6304378e4e90da
SerSe1n <serse1n.sen@gmail.com>
2023-09-25 00:57:01 +0300

    json example

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import re
+import json
 
 dataTypes = r'void|int|double|float|char|string|long'
 
+
 def parse_cpp_file():
     scheme = {'blocks': [], 'arrows': [], 'x0': 0, 'y0': 100}
+    x = 1000
+    y = 100
+
     with open('solve.cpp', 'r') as f:
         commands = list(map(lambda x: x.rstrip().strip(r'[{} ]'), f.readlines()))
         commands = list(map(lambda x: x.lstrip(' ').replace('endl', '').replace('<<', '').replace('>>', '').replace('rand()', 'рандом'), commands))
@@ -14,7 +19,25 @@ def parse_cpp_file():
                 continue
             if re.match(dataTypes, commands[i]) and '(' in commands[i] and ')' in commands[i] \
                 and not ';' in commands[i] and not '=' in commands[i]:
-                print(commands[i])
+                funcName = commands[i][commands[i].find(' ') + 1:]
+                scheme['blocks'].append(
+                    {
+                        'x': x, 
+                        'y': y,
+                        'text': funcName,
+                        'width': 100,
+                        'height': 30,
+                        'type': 'Начало / конец',
+                        'isMenuBlock': False,
+                        'fontSize': 14,
+                        'textHeight': 14,
+                        'isBold': False,
+                        'isItalic': False,
+                        'textAlign': 'center',
+                        'labelsPosition': 1
+                    })
+                with open('some.json', 'w') as jsfile:
+                    json.dump(scheme, jsfile)
 
 
 
@@ -22,5 +45,5 @@ def main():
     parse_cpp_file()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
